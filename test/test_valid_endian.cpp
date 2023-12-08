c4e65da35ae061190ba8201c0d19c3944ddce2c9
chenxi393 <1578477681@qq.com>
2023-12-09 05:21:45 +0800

    update test

--- test/test_valid_endian.cpp
+++ test/test_valid_endian.cpp
@@ -20,8 +20,8 @@ void printByteOrder(const unsigned int value)
 
 void Test()
 {
-    // 0x34 31 31 32
-    __uint8_t rbuf[] = { 52, 49, 49, 50, 0x51, 0x10, 0x00, 0x00 };
+    // 0x34 0x31
+    __uint8_t rbuf[] = { 52, 49, 0, 0, 0x51, 0x10, 0x00, 0x00 };
     __uint32_t len;
 
     // 使用memcpy将rbuf中的前4个字节复制到len中
@@ -30,8 +30,8 @@ void Test()
     // 打印结果
     printf("len: %u\n", len);
     printf("%x\n", len);
-    printf("%d\n", 0x34313132);
-    printf("%d\n", 0x32313134);
+    printf("%d\n", 0x3431);
+    printf("%d\n", 0x3134);
 }
 
 int main()
